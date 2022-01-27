2986c3c29b623e5325ec8a4e074dcfa8936a83f3
Didac <lopezm.didac@gmail.com>
2022-01-28 00:21:15 +0100

    session1

--- kar/kar.py
+++ kar/kar.py
@@ -4,7 +4,6 @@
 #nombre = input()
 #print("Bienvenido " + nombre)  
 
-
 apodo = "kar"
 edad = 20
 altura_m = 1.75
@@ -12,18 +11,45 @@ clase = "clerigo"
 soltero = False
 pareja = soltero
 
+
+entrada_apodo = None
+while entrada_apodo != apodo:
+    print("Hola, cual es tu apodo?")
+    entrada_apodo = input()
+    if entrada_apodo == apodo:    
+        print("Datos personales del aventurero:")
+        print("******** Apodo : " + apodo)
+        print("******** Edad : " + str(edad))
+        print("******** Altura : " + str(altura_m))
+        print("******** Clase : " + clase) 
+        if soltero is True:
+            print("Married : No")
+        if soltero is False:
+            print("******** Tiene pareja ? : Si")
+        
+    elif  entrada_apodo == "Q" or entrada_apodo == "q":
+        break
+    else:
+         print("No eres el elegido")
+
+ 
+
+
+
 #if soltero = True: 
 #    print("Si")
 #    if False:
 #       print("no")
 
-print("Datos personales del aventurero:")
-print("******** Apodo : " + apodo)
-print("******** Edad : " + str(edad))
-print("******** Altura : " + str(altura_m))
-print("******** Clase : " + clase)
-print("******** Tiene pareja ? : " + str(soltero))
-if soltero is True:
-    print("Married : NO")
-if soltero is False:
-    print("Married : YES")
+#print("Datos personales del aventurero:")
+#print("******** Apodo : " + apodo)
+#print("******** Edad : " + str(edad))
+#print("******** Altura : " + str(altura_m))
+#print("******** Clase : " + clase)
+#print("******** Tiene pareja ? : " + str(soltero))
+#if soltero is True:
+    #print("Married : No")
+#if soltero is False:
+    #print("******** Tiene pareja ? : Si")
+
+#if 
