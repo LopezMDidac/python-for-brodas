ba92cd136a00370900ec3c1035f15200034ba067
MarcSNM <marcsaez.navarro@gmail.com>
2022-02-17 22:14:00 +0100

    corriendosaltando

---
+++ markis/pruebitas.py
@@ -0,0 +1,20 @@
+# retocar while para que en impares la cuelta sea "corriendo" y en los pares "saltando"
+# ejemplo de output:
+# 1 --> corriendo
+# 2 --> saltando
+# 3 --> corriendo
+# 4 --> saltando
+# 5 --> corriendo
+
+
+vueltas = 1
+final = 5
+
+# <> =
+
+while vueltas <= final:
+    print(vueltas)
+    vueltas = vueltas + 1
+   
+
+    
