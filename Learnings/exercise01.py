9caeaa32f5f3ce85ca3ff62392d49bb6668cb880
Didac <lopezm.didac@gmail.com>
2022-02-17 22:34:02 +0100

    exercise01

---
+++ Learnings/exercise01.py
@@ -0,0 +1,34 @@
+# Creación de personaje
+# 
+# Hay que hacer un script que permita al usuario la creación de un personaje
+# mediante preguntas y respuestas.
+#
+# Para cada personaje hay que pedir la siguente información:
+#   nombre
+#   edad 
+#   altura
+#   clase
+#   es soltero
+# 
+# Para cada atributo del personaje hay que validar y procesar la la entrada del usuario.
+# En caso que la respuesta del usuario no sea valida hay que volver a pedirla hasta que lo sea
+# La validación se hará de la siguiente manera:
+#
+# nombre.
+#     Validar que el nombre solo contiene texto
+#     Capitalizar cada palabra (primera mayuscula resto minusculas)
+#
+# edad.
+#     Validar que la entrada contiene solo numeros
+#     Guardar el valor en formato int
+#
+# altura. (en metros)
+#     Validar que la entrada tiene es un float valido
+#     Guardar el valor con decimal redondeado a 2 decimales justos
+#
+# clase.
+#     Validar que la clase es una clase valida
+#
+# es soltero
+#     Preguntar si el personaje es soltero
+#     Guardar un valor booleano de si lo es o no.
