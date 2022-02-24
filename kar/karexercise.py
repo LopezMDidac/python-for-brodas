52794a668c898617ab51c9b20057573a639c3941
SokarWriter <oscar.saez.n@gmail.com>
2022-02-24 11:36:42 +0100

    altura y soltero

--- kar/karexercise.py
+++ kar/karexercise.py
@@ -26,17 +26,40 @@
 # altura. (en metros)
 #     Validar que la entrada tiene es un float valido
 #     Guardar el valor con decimal redondeado a 2 decimales justos
-#
+
+intro_altura = "Introduce la altura deseada para tu personaje en metros "
+
+
+print(intro_altura)
+altura_char = input()
+
+try:
+    altura_char = float(altura_char)
+except ValueError:
+    print("La altura de tu personaje tiene que ser un numero ej: 1.85")
+    altura_char = (input())
+
+altura_char = round(altura_char, 2)
+#print de comprobación redondeo
+print("Tu personaje mide: " + str(altura_char) + " metros")
+
+
+
+
+
+
 # clase. (crear una lista de clases con validación)
 #     Validar que la clase es una clase valida
 
 
 
+
+
 clase = ["guerrero", "mago", "arquero"]
-intro = "Elige una clase para empezar tu aventura" + str(clase)
-print(intro)
+intro_clase = "Elige una clase para empezar tu aventura" + str(clase)
 
 
+print(intro_clase)
 chose_class = input()
 check_class = clase.count(chose_class)
 
@@ -51,8 +74,28 @@ while chose_class != clase:
         check_class = clase.count(chose_class)
 
         
-
-#
+    #
 # es soltero
 #     Preguntar si el personaje es soltero
 #     Guardar un valor booleano de si lo es o no.
+
+intro_soltero = "En tu aventura te esposaste con alguna belleza? si o no"
+soltero = None
+
+print(intro_soltero)
+is_soltero = input()
+while is_soltero != "si" or is_soltero != "no":   
+    if is_soltero == "si":
+        soltero = True
+        print("Afortunado aventurero")
+        break
+    
+    if is_soltero == "no":
+        soltero = False
+        print("Mejor solo que mal acompañado")
+        break
+    
+    else:
+        print("Contesta unicamente con si o no")
+        is_soltero = input()
+
