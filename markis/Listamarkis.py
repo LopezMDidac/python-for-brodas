f79f1a222653bb6117ceab50dc88b1d16685ce22
MarcSNM <marcsaez.navarro@gmail.com>
2022-03-20 15:16:43 +0100

    Co-authored-by: Didac Lopez <lopezm.didac@gmail.com> Co-authored-by: SokarWriter <SokarWriter@users.noreply.github.com>

--- markis/Listamarkis.py
+++ markis/Listamarkis.py
@@ -7,37 +7,73 @@
 # > 3. Modificar producto de la lista
 # > 4. Quitar producto de la lista
 # <
-compra = []
 
+def dummy(name:str, num:int)->int:
+    print("holaaa")
+    print(name)
+    print(num)
+    print(num * 3)
+    print("--------------------------------------------------------------------------")
+    return num * 3
+dummy("tomaa", 6)
+resultado = dummy("yaaa", 5 )
+print(resultado)
+
+compra = []
+#error linia 30
 while True:
     print("0. Salir")
     print("1. Ver lista de la compra")
     print("2. Agregar producto a la lista")
     print("3. Modificar producto de la lista")
     print("4. Quitar producto de la lista")
-    elegir_opcion = input()
-    elegir_opcion= int(elegir_opcion)
+    try:
+        elegir_opcion = input()
+        elegir_opcion= int(elegir_opcion)
+    except ValueError:
+        print("seleccion escribiendo el numero")
     if elegir_opcion == 0:
-        print("Has elegido: Salir" + "Que tenga un buen dia")
+        print("Has elegido: Salir " + " Que tenga un buen dia")
         break
-    if elegir_opcion == 1:
+    elif elegir_opcion == 1:
         print(compra)
         print("Que desea hacer?")
-    if elegir_opcion == 2:
+    elif elegir_opcion == 2:
         agregar_producto = input()
         compra.append(agregar_producto)
-    if elegir_opcion == 3:
-        for index, value in enumerate(compra):
-            if value == compra:
-                compra[index] = modificar_producto
+    elif elegir_opcion == 3:
         print(compra)
-        print("Que producto queires modificar?")
         modificar_producto = input()
-    if elegir_opcion == 4:
+        print ("Por que producto desea cambiarlo")
+        cambiar_producto = input()
+        for producto in compra:
+            if modificar_producto == producto:
+                compra.remove(modificar_producto)
+                compra.append(cambiar_producto)
+                print("Has modificado " + modificar_producto + "por" +  cambiar_producto)
+                print(compra)
+                break
+            
+
+    elif elegir_opcion == 4:
         print(compra)
         print ("Que articulo desea quitar de la lista?")
-        borrar = input
-        list.remove(borrar)
+        print ("Escriba el articulo que desea quitar?")
+        try:
+            borrar = input()
+            compra.remove(borrar)
+            print("El producto '" + borrar + "' ha sido eliminado de la lista, quedan: ", len(compra))
+        except ValueError:
+            print("el producto '"  + borrar +  "' no esta en la lista")
+
+    else:
+        print("opcion no valida")
+
+
+
+
+
+
 
 
 
