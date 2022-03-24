ebbd21b99aed37b2bc386a70c5b2cf86293407d4
MarcSNM <marcsaez.navarro@gmail.com>
2022-03-24 22:53:33 +0100

    uptdate llista markis

--- markis/pruebitas.py
+++ markis/pruebitas.py
@@ -31,6 +31,58 @@ while vueltas != final:
 #\"- double quote.
 #\\-Backslash.
 
+
+def dummy(name:str, num:int)->int:
+    print("holaaa")
+    print(name)
+    print(num)
+    print(num * 3)
+    print("--------------------------------------------------------------------------")
+    return num * 3
+dummy("tomaa", 6)
+resultado = dummy("yaaa", 5 )
+print(resultado)
+
+
+def elegir_opcion():
+    print(compra)
+    print("Que desea hacer?")
+
+
+
+def aregar_producto():
+    agregar_producto = input()
+    compra.append(agregar_producto)
+
+
+
+def cambiar_producto():
+    print(compra)
+    modificar_producto = input()
+    print ("Por que producto desea cambiarlo")
+    cambiar_producto = input()
+    for producto in compra:
+        if modificar_producto == producto:
+            compra.remove(modificar_producto)
+            compra.append(cambiar_producto)
+            print("Has modificado " + modificar_producto + "por" +  cambiar_producto)
+            print(compra)
+            break
+
+def borrar_producto():
+    print(compra)
+    print ("Que articulo desea quitar de la lista?")
+    print ("Escriba el articulo que desea quitar?")
+    try:
+        borrar = input()
+        compra.remove(borrar)
+        print("El producto '" + borrar + "' ha sido eliminado de la lista, quedan: ", len(compra))
+    except ValueError:
+            print("el producto '"  + borrar +  "' no esta en la lista")
+
+    else:
+        print("opcion no valida")
+
     
 
 
